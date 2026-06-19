428a5c58c56d21f8a68c4b1b0609927874e42d25
PRABHUNAGAMANI <prabhu.nagamani98@gmail.com>
2026-06-19 12:36:38 +0530

    FEAT : add Train Controller

--- TrainConsistManagement/src/controller/TrainController.java
+++ TrainConsistManagement/src/controller/TrainController.java
@@ -4,20 +4,41 @@ package controller;
 import service.TrainService;
 import service.TrainServiceImpl;
 
+/**
+ * Controller layer responsible for handling application flow.
+ *
+ * <p>This class acts as a mediator between:
+ * <ul>
+ *     <li>Application entry point (Main)</li>
+ *     <li>Business logic layer (Service)</li>
+ * </ul>
+ *
+ * <p>In UC3, it triggers the unique bogie ID tracking workflow.
+ *
+ * @author Developer
+ * @version 3.0
+ */
 public class TrainController {
 
     private final TrainService trainService;
 
+    /**
+     * Default constructor initializes service dependency.
+     */
     public TrainController() {
         this.trainService = new TrainServiceImpl();
     }
 
-    public void runUC2() {
+    /**
+     * Executes Use Case 3:
+     * Track unique bogie IDs using HashSet.
+     */
+    public void runUC3() {
 
         System.out.println("=======================================");
-        System.out.println("UC2 - Add Passenger Bogies to Train");
+        System.out.println("UC3 - Track Unique Bogie IDs");
         System.out.println("=======================================\n");
 
-        trainService.performPassengerBogieOperations();
+        trainService.trackUniqueBogieIds();
     }
 }
