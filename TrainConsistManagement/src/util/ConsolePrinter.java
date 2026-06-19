67af86689e49b4e0f37b467bdb41ac68483e52cd
PRABHUNAGAMANI <prabhu.nagamani98@gmail.com>
2026-06-19 11:53:57 +0530

    FEAT : add Train Console Printer

---
+++ TrainConsistManagement/src/util/ConsolePrinter.java
@@ -0,0 +1,14 @@
+package com.train.util;
+
+public class ConsolePrinter {
+
+    public static void printBanner() {
+        System.out.println("=======================================");
+        System.out.println("=== Train Consist Management App ===");
+        System.out.println("=======================================\n");
+    }
+
+    public static void printReadyMessage() {
+        System.out.println("\nSystem ready for operations...");
+    }
+}
