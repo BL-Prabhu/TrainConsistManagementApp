daa0ccac82990914fe442b92e1733913f4518ba5
PRABHUNAGAMANI <prabhu.nagamani98@gmail.com>
2026-06-19 12:20:26 +0530

    FEAT : add Train Service Implementation

--- TrainConsistManagement/src/service/TrainServiceImpl.java
+++ TrainConsistManagement/src/service/TrainServiceImpl.java
@@ -1,21 +1,41 @@
-package com.train.service.impl;
+package service;
 
-import com.train.model.Train;
-import com.train.service.TrainService;
+
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class TrainServiceImpl implements TrainService {
 
-    private Train train;
-
     @Override
-    public void initializeTrain() {
-        this.train = new Train();
-        System.out.println("Train initialized successfully...");
-    }
+    public void performPassengerBogieOperations() {
 
-    @Override
-    public void displaySummary() {
-        System.out.println("Initial Bogie Count : " + train.getBogieCount());
-        System.out.println("Current Train Consist : " + train.getBogies());
+        // Create list
+        List<String> passengerBogies = new ArrayList<>();
+
+        // ADD bogies
+        passengerBogies.add("Sleeper");
+        passengerBogies.add("AC Chair");
+        passengerBogies.add("First Class");
+
+        System.out.println("After Adding Bogies:");
+        System.out.println("Passenger Bogies : " + passengerBogies);
+
+        // REMOVE bogie
+        passengerBogies.remove("AC Chair");
+
+        System.out.println("\nAfter Removing 'AC Chair':");
+        System.out.println("Passenger Bogies : " + passengerBogies);
+
+        // CHECK existence
+        System.out.println("\nChecking if 'Sleeper' exists:");
+        boolean exists = passengerBogies.contains("Sleeper");
+        System.out.println("Contains Sleeper? : " + exists);
+
+        // FINAL OUTPUT
+        System.out.println("\nFinal Train Passenger Consist:");
+        System.out.println(passengerBogies);
+
+        System.out.println("\nUC2 operations completed successfully...");
     }
 }
