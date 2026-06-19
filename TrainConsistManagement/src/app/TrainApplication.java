ba3cc459d1fa82a1e4d5664605930edb3fbc43f6
PRABHUNAGAMANI <prabhu.nagamani98@gmail.com>
2026-06-19 12:37:21 +0530

    FEAT : add Main Method

--- TrainConsistManagement/src/app/TrainApplication.java
+++ TrainConsistManagement/src/app/TrainApplication.java
@@ -4,14 +4,34 @@ package app;
 import controller.TrainController;
 import util.ConsolePrinter;
 
+/**
+ * Entry point of the Train Consist Management Application.
+ *
+ * <p>This class is responsible for:
+ * <ul>
+ *     <li>Bootstrapping the application</li>
+ *     <li>Invoking the controller layer</li>
+ *     <li>Displaying application banner and ready state</li>
+ * </ul>
+ *
+ * <p>Follows standard Java application structure using the main() method.
+ *
+ * @author Developer
+ * @version 3.0
+ */
 public class TrainApplication {
 
+    /**
+     * Main method - JVM entry point.
+     *
+     * @param args command-line arguments
+     */
     public static void main(String[] args) {
 
         ConsolePrinter.printBanner();
 
         TrainController controller = new TrainController();
-        controller.runUC2();   // calling UC2
+        controller.runUC3();
 
         ConsolePrinter.printReadyMessage();
     }
