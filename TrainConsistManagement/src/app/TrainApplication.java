3289f06bbdc431abce4287712d12ee2e95534c26
PRABHUNAGAMANI <prabhu.nagamani98@gmail.com>
2026-06-20 12:52:30 +0530

    FEAT : add Main Method

--- TrainConsistManagement/src/app/TrainApplication.java
+++ TrainConsistManagement/src/app/TrainApplication.java
@@ -2,23 +2,31 @@ package app;
 
 import controller.BogieController;
 
+import java.util.List;
+import java.util.Map;
+
 public class TrainApplication {
 
     public static void main(String[] args) {
 
         BogieController controller = new BogieController();
 
-        System.out.println("=== UC8: Filter Bogies using Streams ===");
+        System.out.println("=== UC9: Group Bogies by Type ===");
 
-        // Reuse data
-        controller.addBogie("Sleeper", 72);
-        controller.addBogie("AC Chair", 56);
-        controller.addBogie("First Class", 24);
-        controller.addBogie("General", 90);
+        // Add Bogies
+        controller.addBogie("Sleeper", 72, "Passenger");
+        controller.addBogie("AC Chair", 56, "Passenger");
+        controller.addBogie("First Class", 24, "Passenger");
+        controller.addBogie("Goods Carrier", 0, "Goods");
+        controller.addBogie("Parcel Van", 0, "Goods");
 
-        System.out.println("\nBogies with capacity > 60:");
+        // Grouping
+        Map<String, List<model.Bogie>> grouped = controller.getGroupedBogies();
 
-        controller.getFilteredBogies(60)
-                .forEach(System.out::println);
+        // Print grouped result
+        grouped.forEach((type, bogies) -> {
+            System.out.println("\nType: " + type);
+            bogies.forEach(System.out::println);
+        });
     }
 }
