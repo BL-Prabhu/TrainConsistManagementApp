86b66821e573b81ed6b1be17f7b5005b851affeb
PRABHUNAGAMANI <prabhu.nagamani98@gmail.com>
2026-06-20 11:55:19 +0530

    FEAT : add Bogie Service Interface

---
+++ TrainConsistManagement/src/service/BogieService.java
@@ -0,0 +1,15 @@
+package service;
+
+import model.Bogie;
+import java.util.List;
+
+public interface BogieService {
+
+    void addBogie(Bogie bogie);
+
+    List<Bogie> getAllBogies();
+
+    List<Bogie> sortByCapacityAscending();
+
+    List<Bogie> sortByCapacityDescending();
+}
