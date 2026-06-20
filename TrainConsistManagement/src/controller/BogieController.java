1bdd53723fbbe9e2a1819ad2e00142d1149a1636
PRABHUNAGAMANI <prabhu.nagamani98@gmail.com>
2026-06-20 11:54:28 +0530

    FEAT : add Bogie Controller

---
+++ TrainConsistManagement/src/controller/BogieController.java
@@ -0,0 +1,32 @@
+package controller;
+
+import model.Bogie;
+import service.BogieService;
+import service.BogieServiceImpl;
+
+import java.util.List;
+
+public class BogieController {
+
+    private final BogieService service;
+
+    public BogieController() {
+        this.service = new BogieServiceImpl();
+    }
+
+    public void addBogie(String name, int capacity) {
+        service.addBogie(new Bogie(name, capacity));
+    }
+
+    public List<Bogie> getAllBogies() {
+        return service.getAllBogies();
+    }
+
+    public List<Bogie> getSortedAscending() {
+        return service.sortByCapacityAscending();
+    }
+
+    public List<Bogie> getSortedDescending() {
+        return service.sortByCapacityDescending();
+    }
+}
